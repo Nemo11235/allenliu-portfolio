e8798d32cd51af99f3b917b3ea27e8fb6ff28289
Allen Liu <liuzhexian11235@gmail.com>
2023-03-23 11:42:44 -0700

    navigation bar done
    also added kitty pictures and a thank you page for testing

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./HomePage/HomePage";
 import ThankPage from "./ThankPage/ThankPage";
+import paths from "./Utils/paths";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/thank" element={<ThankPage />} />
+        <Route path={paths.home} element={<HomePage />} />
+        <Route path={paths.thank} element={<ThankPage />} />
       </Routes>
     </Router>
   );
--- src/Utils/paths.js
+++ src/Utils/paths.js
@@ -1,9 +1,9 @@
 // this file contains all the path names for each page.
 const paths = {
-  home: `/`,
-  contact: `/contact`,
-  projects: `/projects`,
-  thank: `/thank`,
+  home: `/allenliu-portfolio/`,
+  contact: `/allenliu-portfolio/contact`,
+  projects: `/allenliu-portfolio/thankprojects`,
+  thank: `/allenliu-portfolio/thank`,
 };
 
 export default paths;
