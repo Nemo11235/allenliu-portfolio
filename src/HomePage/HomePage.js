10bfe78ca7be8e36ad4f4474822884bf934a4d7e
Allen Liu <liuzhexian11235@gmail.com>
2023-03-23 12:27:23 -0700

    Update HomePage.js

--- src/HomePage/HomePage.js
+++ src/HomePage/HomePage.js
@@ -6,7 +6,7 @@ function HomePage() {
   return (
     <div className="home-div">
       <NavBar />
-      <h1 className="welcome-msg">Welcome to My Portfolio!</h1>
+      <h1 className="welcome-msg">Welcome to Allen's Portfolio!</h1>
       <div className="intro">
         <p>
           Hi, my name is Allen. This portfolio showcases my skills, projects,
