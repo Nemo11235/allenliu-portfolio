4b2c3d4882cd769adf82f9710cb7cfa716a146b7
Allen Liu <liuzhexian11235@gmail.com>
2023-03-23 13:18:36 -0700

    added pic

--- src/ThankPage/ThankPage.js
+++ src/ThankPage/ThankPage.js
@@ -1,11 +1,16 @@
 import React from "react";
 import NavBar from "../NavBar/NavBar";
+import BothImg from "../Images/both.jpg";
+import "./ThankPage.scss";
 
 function ThankPage() {
   return (
     <div>
       <NavBar />
       <h1>My cats</h1>
+      <div className="img-wrapper">
+        <img src={BothImg} className="img-style" alt="An img of both cats" />
+      </div>
     </div>
   );
 }
