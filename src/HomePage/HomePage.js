170d89ebf8ac3bc86873af01f2930f5b160f7de8
Allen Liu <liuzhexian11235@gmail.com>
2023-03-23 22:36:58 -0700

    animation

--- src/HomePage/HomePage.js
+++ src/HomePage/HomePage.js
@@ -4,17 +4,16 @@ import NavBar from "../NavBar/NavBar";
 
 function HomePage() {
   return (
-    <div className="home-div">
+    <div className="root">
       <NavBar />
       <div className="welcome-div">
-        <h1 className="welcome-msg">Hi, my name is Allen</h1>
-        <div className="intro">
-          <p>
-            Hi, my name is Allen. This portfolio showcases my skills, projects,
-            and experience.
-          </p>
+        <div className="welcome-info">
+          <h1 className="welcome-msg">Hi, my name is Allen</h1>
+          <div className="intro">
+            <p>This portfolio showcases my skills, projects, and experience.</p>
+          </div>
+          <button className="btn-viewProject">View Projects</button>
         </div>
-        <button className="btn-viewProject">View Projects</button>
       </div>
     </div>
   );
