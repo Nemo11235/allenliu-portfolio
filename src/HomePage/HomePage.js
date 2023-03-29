47764e0c8ba2c21a26b0f4c7a5adb0a09ae54d7e
Allen Liu <liuzhexian11235@gmail.com>
2023-03-28 17:02:56 -0700

    update

--- src/HomePage/HomePage.js
+++ src/HomePage/HomePage.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./HomePage.scss";
 import NavBar from "../NavBar/NavBar";
+import Rellax from "rellax";
 
 function HomePage() {
   function scrollToAbout() {
@@ -8,37 +9,64 @@ function HomePage() {
     element.scrollIntoView({ behavior: "smooth" });
   }
 
+  function scrollToTop() {
+    const element = document.getElementsByClassName("root")[0];
+    element.scrollIntoView({ behavior: "smooth" });
+  }
+
+  useEffect(() => {
+    new Rellax(".rellax");
+  }, []);
+
   return (
     <div className="root">
+      <button class="back-top" onClick={scrollToTop}>
+        <span class="circle" aria-hidden="true">
+          <span class="icon arrow"></span>
+        </span>
+        <span class="button-text">Back to top</span>
+      </button>
       <NavBar />
       <div className="welcome-card">
         <div className="welcome-info">
-          <h1 className="welcome-msg">Hi, my name is Allen</h1>
-          <div className="intro">
-            <p>This portfolio showcases my skills, projects, and experience.</p>
+          <div className="rellax" data-rellax-speed="5">
+            <h1 className="welcome-msg">Hi, my name is Allen</h1>
           </div>
-          <button className="btn-toAbout" onClick={scrollToAbout}>
-            Get started <span class="icon">&#10148;</span>
-          </button>
+          <div className="intro">
+            <div className="rellax" data-rellax-speed="3">
+              <p>
+                This portfolio showcases my skills, projects, and experience.
+              </p>
+            </div>
+          </div>
+          <div className="rellax" data-rellax-speed="1">
+            <button className="btn-toAbout" onClick={scrollToAbout}>
+              Get started <span class="icon">&#10148;</span>
+            </button>
+          </div>
         </div>
       </div>
-
       <div className="intro-card">
         <div className="intro-left">
+          <div className="selfie-container"></div>
           <div className="selfie"></div>
           <h1>Allen Liu</h1>
         </div>
         <div className="intro-right">
-          <div className="self-intro-contianer">
+          <div className="self-intro-container">
             <h1>About Me</h1>
             <p>
               Hello, I'm an immigrant from China who moved to the US in 2016.
               Since then, I've been pursuing my passion for computer science and
-              making new friends in this exciting new environment. I'm also a
-              proud owner of two adorable cats named MeiMei and XiaoHei, or as
-              they're affectionately known, Pusheen and Blackie. Recently, I
-              graduated from SJSU and am eager to launch my career with a
-              fulfilling full-time job. As someone who is enthusiastic,
+              making new friends in this exciting new environment.
+            </p>
+            <p>
+              I'm also a proud owner of two adorable cats named MeiMei and
+              XiaoHei, or as they're affectionately known, Pusheen and Blackie.{" "}
+            </p>
+            <p>
+              Recently, I graduated from SJSU and am eager to launch my career
+              with a fulfilling full-time job. As someone who is enthusiastic,
               diligent, and always eager to learn, I'm confident that I can make
               a valuable contribution to any team. Thank you for taking the time
               to get to know me!{" "}
@@ -50,9 +78,9 @@ function HomePage() {
         <div className="quote-container">
           <h1>
             We should consider every day lost in which we have not danced at
-            least once
+            least once.
           </h1>
-          <h2>———— Friedrich Nietzsche</h2>
+          <h2>———— Friedrich Wilhelm Nietzsche</h2>
         </div>
       </div>
       <div className="project-card">
