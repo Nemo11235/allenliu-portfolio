14e38e4776bb8dcc2bcb0e43d677cdd239541b0e
Allen Liu <liuzhexian11235@gmail.com>
2024-03-13 21:08:35 -0700

    added img slider for special thank page

--- src/ProjectsPage/ProjectPage.js
+++ src/ProjectsPage/ProjectPage.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./ProjectPage.scss";
 import SocialBar from "../Components/SocialBar/SocialBar";
 import NavBar from "../Components/NavBar/NavBar";
+
 function ProjectPage() {
   return (
     <div className="root">
